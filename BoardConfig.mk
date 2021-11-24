ff4a55908141d590ee0ab4bae35f35eac4965381
micky387 <mickaelsaibi@free.fr>
2021-11-24 21:51:41 +0100

    zenfone6: replace deprecated sepolicy BOARD_PLAT_ by SYSTEM_EXT_
    Change-Id: I68b4e0c57686393f01ce0d27e4437eaea477f1f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ TARGET_USES_MKE2FS := true
 # Sepolicy
 include vendor/omni/sepolicy/sepolicy.mk
 include $(DEVICE_PATH)/sepolicy/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private \
                                    $(DEVICE_PATH)/sepolicy/qva/private
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/product/private
 
