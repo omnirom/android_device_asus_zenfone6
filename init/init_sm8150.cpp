46babe15ec04def63ea33be3b77b22247a2214e0
micky387 <mickaelsaibi@free.fr>
2023-05-17 16:32:05 +0200

    zenfone6: Override more props and on all source partitions
    Inspired by https://github.com/LineageOS/android_device_oneplus_dre/commit/16981a495a0a33a4698e91859f14206b3fba8ada
    
    Change-Id: Ib30b1fc5bb6fcf0f9f4f66b2895713f4ed1d8e17

--- init/init_sm8150.cpp
+++ init/init_sm8150.cpp
@@ -73,6 +73,32 @@ void property_override_dual(char const prop[], char const system_prop[],
     property_override(system_prop, value);
 }
 
+void ro_prop_override(char const source[], char const prop[],
+    const std::string& value, bool product)
+{
+    std::string prop_name = "ro.";
+
+    if (product) prop_name += "product.";
+    if (source != nullptr) prop_name += source;
+    if (!product) prop_name += "build.";
+    prop_name += prop;
+
+    property_override(prop_name.c_str(), value);
+};
+
+static const char* RO_PROP_SOURCES[] = {
+    nullptr,
+    "bootimage.",
+    "odm.",
+    "odm_dlkm.",
+    "product.",
+    "system.",
+    "system_dlkm.",
+    "system_ext.",
+    "vendor.",
+    "vendor_dlkm.",
+};
+
 /* From Magisk@jni/magiskhide/hide_utils.c */
 static const char *snet_prop_key[] = {
     "ro.boot.vbmeta.device_state",
@@ -108,12 +134,15 @@ static void set_configs()
 
     if (ReadFileToString(CUSTOMER_FILE, &variantValue)) {
         property_override("ro.config.CID", variantValue.c_str());
+        property_override("ro.vendor.config.CID", variantValue.c_str());
     }
     if (ReadFileToString(IDCODE_FILE, &variantValue)) {
         property_override("ro.config.idcode", variantValue.c_str());
+        property_override("ro.vendor.config.idcode", variantValue.c_str());
     }
     if (ReadFileToString(VARIANT_FILE, &variantValue)) {
         property_override("ro.config.versatility", variantValue.c_str());
+        property_override("ro.vendor.config.versatility", variantValue.c_str());
     }
 }
 
@@ -176,8 +205,18 @@ static void set_fingerprint()
     property_override("ro.build.asus.version", build_asus);
     property_override("ro.build.description", description);
     property_override("ro.build.display.id", display_id);
+    property_override("ro.product.name", name);
     property_override("ro.vendor.asus.build.fp", fingerprint);
-    property_override_dual("ro.build.fingerprint", "ro.system.build.fingerprint", fingerprint);
+
+    for (const auto& source : RO_PROP_SOURCES) {
+        ro_prop_override(source, "brand", "asus", true);
+        ro_prop_override(source, "device", "ASUS_I01WD", true);
+        ro_prop_override(source, "manufacturer", "asus", true);
+        ro_prop_override(source, "model", "ASUS_I01WD", true);
+        ro_prop_override(source, "name", name, true);
+
+        ro_prop_override(source, "fingerprint", fingerprint, false);
+    }
 }
 
 void vendor_load_properties()
