2159a13baebb62d2fcb3ac1e0123547ba7b4dfb7
micky387 <mickaelsaibi@free.fr>
2023-12-10 13:26:54 +0100

    zenfone6: Build Fingerprint 2.1 HAL from source
    Change-Id: Ia80315b03c9838b8c576e7503153759bb52b17f3

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Camera
 PRODUCT_PACKAGES += \
     libgui_shim
